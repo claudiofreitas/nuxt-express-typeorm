1c51dd86932b59b001aedb49e2bb8c1b9fb7c87b
Marcuz <macoycorpuz@gmail.com>
2021-04-12 15:05:35 +0900

    backend: working device crud operations

--- server/src/device/device.controller.ts
+++ server/src/device/device.controller.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response, Router } from 'express'
+import { getRepository } from 'typeorm'
+import DeviceNotFoundException from '../exceptions/DeviceNotFoundException'
 import Controller from '../interfaces/controller.interface'
+import CreateDeviceDto from './device.dto'
+import Device from './device.entity'
 
 class DeviceController implements Controller {
   public path = '/devices'
   public router = Router()
+  private deviceRepository = getRepository(Device)
 
   constructor() {
     this.initializeRoutes()
@@ -18,10 +23,11 @@ class DeviceController implements Controller {
   }
 
   private getAllDevices = async (
-    request: Request,
+    _request: Request,
     response: Response
   ) => {
-    // TODO
+    const devices = await this.deviceRepository.find()
+    response.send(devices)
   }
 
   private getDeviceById = async (
@@ -29,21 +35,37 @@ class DeviceController implements Controller {
     response: Response,
     next: NextFunction
   ) => {
-    // TODO
+    const id = request.params.id
+    const device = await this.deviceRepository.findOne(id)
+    if (device) {
+      response.send(device)
+    } else {
+      next(new DeviceNotFoundException(id))
+    }
   }
 
   private createDevice = async (
     request: Request,
     response: Response
   ) => {
-    // TODO
+    const deviceData: CreateDeviceDto = request.body
+    const newDevice = this.deviceRepository.create(deviceData)
+    await this.deviceRepository.save(newDevice)
+    response.send(newDevice)
   }
 
+  /*
+   * This api will return 204 regardless of the result.
+   * There is a bug in TypeORM when using sqlite.
+   * Issue: https://github.com/typeorm/typeorm/issues/2415#issuecomment-455263074
+   */
   private deleteDevice = async (
     request: Request,
     response: Response
   ) => {
-    // TODO
+    const id = request.params.id
+    await this.deviceRepository.delete(id)
+    response.sendStatus(204)
   }
 
   private modifyDevice = async (
@@ -51,7 +73,15 @@ class DeviceController implements Controller {
     response: Response,
     next: NextFunction
   ) => {
-    // TODO
+    const id = request.params.id
+    const postData: Device = request.body
+    await this.deviceRepository.update(id, postData)
+    const updatedPost = await this.deviceRepository.findOne(id)
+    if (updatedPost) {
+      response.send(updatedPost)
+    } else {
+      next(new DeviceNotFoundException(id))
+    }
   }
 
 }
